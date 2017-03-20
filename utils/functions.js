00726d83d71b78a06d777cc42d79fc7315f9e8d3
Janos Gyerik <janos.gyerik@gmail.com>
2017-03-20 07:14:56 +0100

    Update functions.js

--- utils/functions.js
+++ utils/functions.js
@@ -1,12 +1,12 @@
 // Every object is linked to a prototype object from which it can inherit properties.
 // All objects created from object literals are linked to `Object.prototype`,
-// an object comes standard with JavaScript.
+// an object that comes standard with JavaScript.
 //
 // When you make a new object, you can select the objet that should be its prototype.
 // The mechanism that JavaScript providers to do this is messy and complex,
 // but it can be significantly simplified.
 //
-// This create method creates a new object that uses an old obejct as its prototype.
+// This `create` method creates a new object that uses an old obejct as its prototype.
 //
 if (typeof Object.create !== 'function') {
   Object.create = function (o) {
